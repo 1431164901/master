05b57b3027410cdaaf38fadb2394480fff4f451b
dengqiao <dengqiao@qq.com>
2019-06-23 14:31:34 +0800

    本地工程添加到git

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -7,6 +7,12 @@ public class FirstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
         System.out.println("hello git project");
+
+    }
+
+    public void test(){
+        int i = 0;
+        i += 10;
     }
 }
 
